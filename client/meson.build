419446aca719d278c0461901e6336ba5e4b85faa
amandx36 <xamandeep360@gmail.com>
2026-05-17 18:39:27 +0530

    Added License

---
+++ client/meson.build
@@ -0,0 +1,3 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2026 Aman Deep (amdeep.dev@gmail.com)
+// SPDX-FileCopyrightText: 2026 Ashish Kumar <15678ashishk@gmail.com>
--- daemon/main.c
+++ daemon/main.c
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2026 Aman Deep (amdeep.dev@gmail.com)
+// SPDX-FileCopyrightText: 2026 Ashish Kumar <15678ashishk@gmail.com>
 #include <unistd.h>
 #include <sys/ioctl.h>
 
